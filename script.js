90dd8b5a650c12ef8da391066368562e10b7bf3e
alex w <awaite67@gmail.com>
2022-06-16 17:09:46 -0400

    worked on holder function;

--- script.js
+++ script.js
@@ -22,18 +22,6 @@ const buttonEquals = document.querySelector('#item20');
 const button = document.querySelectorAll('button')
 let calcDisplay = document.querySelector('.calcDisplay')
 
-// assign integer values to buttons; not nessecary with value attribute
-// button1.addEventListener('click', () => 1);
-// button2.addEventListener('click', () => 2);
-// button3.addEventListener('click', () => 3);
-// button4.addEventListener('click', () => 4);
-// button5.addEventListener('click', () => 5);
-// button6.addEventListener('click', () => 6);
-// button7.addEventListener('click', () => 7);
-// button8.addEventListener('click', () => 8);
-// button9.addEventListener('click', () => 9);
-// button0.addEventListener('click', () => 0);
-
 // 4 operations for the items
 let addition = (a, b) => a + b
 let subtract = (a, b) => a - b
@@ -41,41 +29,41 @@ let multiply = (a, b) => a * b
 let division = (a, b) => a / b
 
 // assign operation buttons
-buttonAddition.addEventListener('click', () => addition )
+buttonAddition.addEventListener('click', () => addition)
 buttonSubtract.addEventListener('click', () => subtract)
 buttonMultiply.addEventListener('click', () => multiply)
 buttonDivide.addEventListener('click', () => division)
 
 // assign clear and equals button
-buttonClear.addEventListener('click', () => 'return')
 buttonEquals.addEventListener('click', () => operate)
 
 
-// integer holder function and runthru process
+// integer holder function
 const intButtons = document.querySelectorAll('.intButton')
 
-// adding to numHolder1
-let numHolder1 = ''
 
+// assigns holdNum function to intButtons allowing numbers to concencate
+// bind method allows to add outer functions into inner function before calling them.
 
+let holder = 0
 
 for (i = 0; i < intButtons.length; i++){
-  intButtons[i].addEventListener('click', addNum)
-  function addNum(){
-    numHolder1 = numHolder1+intButtons[i].value
-    calcDisplay.innerHTML = numHolder1
-  }
+  intButtons[i].addEventListener('click', holdNum.bind(this, intButtons[i]));
+  function holdNum(num){
+    if (holder.length <= 14) {
+    holder = holder+num.value
+    calcDisplay.innerHTML = holder
+  }}
 };
 
 
-//adding to numHolder2
-// let addNum2 = function(b){
-//   return numHolder2 = numHolder2+b
-// }
-// for (i = 0; i < intButtons.length; i++){
-//   intButtons[i].addEventListener('click', () => addNum2(intButtons[i].value)
-//   )};
+//functionality for clear button
+function clearHolder(){
+  holder = ''; 
+  calcDisplay.innerHTML= holder;
+}
 
+buttonClear.addEventListener('click', clearHolder)
 
 // function operate
 let operate = function(a,b,c){
